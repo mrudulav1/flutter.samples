840428a59fde57d205d26bdc96ca5211284a0b53
mrudula <mrudulavaji11@gmail.com>
2024-03-21 10:33:43 +0530

    edit import line

--- bin/basics_dart.dart
+++ bin/basics_dart.dart
@@ -1,9 +1,32 @@
+// '''
+// import 'dart:io';
+// // put var to declare the type of variablrs
+// void main()
+// {
+//   print("enter your name:");
+//   var a = stdin.readLineSync();
+//   print("hello $a welcome flutter!");
+// }
+// '''
+
+// 2.enter two numbers printing
 
 import 'dart:io';
-// put var to declare the type of variablrs
-void main()
-{
-  print("enter your name:");
-  var a = stdin.readLineSync();
-  print("hello $a welcome flutter!");
-}
+void main(){
+  print("enter two number");
+  var input1=stdin.readLineSync();
+  var input2=stdin.readLineSync();
+
+  var num1=int.parse(input1!);
+  var num2=int.parse(input2!);
+  
+  print('SUM=${num1+num2}');
+  }
+    // '''EXCEPTION HANDLING
+  //  ! using to check null or not..here input1 is not null . so give ! to the code
+  // var num1=int.parse(input1!);
+  //  without using ! check null
+  //  if (input1==null)
+  // {
+  //   input1='0';
+  // }'''
