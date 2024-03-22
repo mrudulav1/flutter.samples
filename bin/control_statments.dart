a2600fd6a570cce15b553f1e09ff051d03dbfda7
mrudula <mrudulavaji11@gmail.com>
2024-03-22 08:35:11 +0530

    create new page for control statments

---
+++ bin/control_statments.dart
@@ -0,0 +1,13 @@
+
+import 'dart:io';
+void main(){
+    int num1=10;
+    if(num1>0)
+    {
+        print("positive");
+    }else
+    {
+        print("negative");
+    }
+    
+}
---
+++ bin/datatypes.dart
@@ -0,0 +1,14 @@
+// int a=10;
+// double b=20;
+// num num1=20.0;
+// num num2=10;
+
+
+// string
+import 'dart:io';
+void main(){
+
+String name='mrudula';
+print(name.length);
+}
+
